fe05bf15c43b8d88ce49c09855299171c00918a7
ben.li <53158397+liberaterb@users.noreply.github.com>
2021-01-21 00:14:38 +0800

    Update 2021-01.js

--- 2021-01.js
+++ 2021-01.js
@@ -15,4 +15,32 @@ var text20210120 =
 9. ★★☆☆☆ 小程序
 10. ★★☆☆☆ react
 11. ★☆☆☆☆ electron
+
+## 复习vue-router
+### 基础
+* HTML: 
+  * <!-- 路由匹配到的组件将渲染在这里 -->
+  * <router-view></router-view>
+* JS
+  模块化编程 -> Vue.use(VueRouter)
+  ``` javascript
+  const routes = [
+    { path: '/foo', component: Foo },
+    { path: '/bar', component: Bar }
+  ]
+
+  // 3. 创建 router 实例，然后传 `routes` 配置
+  // 你还可以传别的配置参数, 不过先这么简单着吧。
+  const router = new VueRouter({
+    routes // (缩写) 相当于 routes: routes
+  })
+
+  // 4. 创建和挂载根实例。
+  // 记得要通过 router 配置参数注入路由，
+  // 从而让整个应用都有路由功能
+  const app = new Vue({
+    router
+  }).$mount('#app')
+  ```
+
 `
