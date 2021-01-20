45b3d7b5833417e0d39feabd568db698ae0bdf68
ben.li <53158397+liberaterb@users.noreply.github.com>
2021-01-21 00:47:54 +0800

    2

--- 2021-01.js
+++ 2021-01.js
@@ -18,18 +18,21 @@ var text20210120 =
 
 ## 复习vue-router
 ### 基础
-* HTML: 
-  * <!-- 路由匹配到的组件将渲染在这里 -->
-  * <router-view></router-view>
+* HTML:
+  ~~~ html
+  <!-- 路由匹配到的组件将渲染在这里 -->
+  <router-view></router-view>
+  ~~~
+  
 * JS
   模块化编程 -> Vue.use(VueRouter)
-  ``` javascript
+  ~~~ javascript
   const routes = [
     { path: '/foo', component: Foo },
     { path: '/bar', component: Bar }
   ]
 
-  // 3. 创建 router 实例，然后传 `routes` 配置
+  // 3. 创建 router 实例，然后传 'routes' 配置
   // 你还可以传别的配置参数, 不过先这么简单着吧。
   const router = new VueRouter({
     routes // (缩写) 相当于 routes: routes
@@ -41,6 +44,6 @@ var text20210120 =
   const app = new Vue({
     router
   }).$mount('#app')
-  ```
+  ~~~
 
 `
