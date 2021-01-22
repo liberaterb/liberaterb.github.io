9af06f482526255264efa7a208251a2f3affb104
ben.li <53158397+liberaterb@users.noreply.github.com>
2021-01-22 22:35:52 +0800

    Update 20210122.js

--- note/20210122.js
+++ note/20210122.js
@@ -1,1 +1,21 @@
-console.log(1)
+var text20210122_01 = 
+`
+# css position
+
+# 常规流
+
+1. static <span style="color: #2ea44f">遵循</span>常规流 top left right bottom 不会起作用
+
+2. relative <span style="color: #2ea44f">遵循</span>常规流 参照常规流的位置通过 top left right bottom 属性进行偏移不影响常规流的任何元素
+
+3. absolute <span style="color: #d73a49">脱离</span>常规流 使用 top left right bottom 进行绝对定位，盒子的偏移不影响常规流的任何元素 其margin不与其他任何margin折叠
+
+4. fixed <span style="color: #d73a49">脱离</span>常规流 使用 top left right bottom 以窗口为参考点进行定位 ，当有滚动条时，对象不会随着滚动
+
+5. sticky 对象在常态时遵循常规流。它就像是 relative 和 fixed 的合体，当在屏幕中时按常规流排版，当卷动到屏幕外时则表现如fixed。该属性的表现是现实中你见到的吸附效果。
+`
+
+var text20210122_02 = 
+`
+# 123
+`
