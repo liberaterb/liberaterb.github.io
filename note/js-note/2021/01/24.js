5a93ffceb285eb93d5e78dcbd8ee91014e11432d
ben.li <53158397+liberaterb@users.noreply.github.com>
2021-01-24 20:23:18 +0800

    Update 24.js

--- note/js-note/2021/01/24.js
+++ note/js-note/2021/01/24.js
@@ -128,3 +128,40 @@ function printVal(val: string): void {
 // 但，声明一个void类型的变量并没有什么大用场，只能赋值undefined和null
 ~~~
 `
+  
+var text20210124_03 = 
+`
+// Null 和 Undefined
+// 在TS里这是 null 和 undefined 对应的两个类型，和 void 类似，他们本身的类型用处不大
+let u: undefined = undefined
+let n: null = null
+
+// 默认下：可以把null和undefined赋值给任何类型，因为它们是所有类型的子类型
+// 但是，如果指定了 --strictNullChecks 标记，null和undefined只能分别赋值给null类型和undefined类型，以及可以赋值为void
+// 推荐使用 --strictNullChecks
+
+// Never
+// 
+/*Never
+never类型表示的是那些永不存在的值的类型。 例如，never类型是那些总是会抛出异常或根本就不会有返回值的函数表达式或箭头函数表达式的返回值类型； 变量也可能是never类型，当它们被永不为真的类型保护所约束时。
+
+never类型是任何类型的子类型，也可以赋值给任何类型；然而，没有类型是never的子类型或可以赋值给never类型（除了never本身之外）。 即使any也不可以赋值给never。
+
+下面是一些返回never类型的函数：*/
+
+// 返回never的函数必须存在无法达到的终点
+function error(message: string): never {
+    throw new Error(message);
+}
+
+// 推断的返回值类型为never
+function fail() {
+    return error("Something failed");
+}
+
+// 返回never的函数必须存在无法达到的终点
+function infiniteLoop(): never {
+    while (true) {
+    }
+}
+`
