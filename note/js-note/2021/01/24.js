9a4f194bd30ecaa4e94364e01507a79fe726b074
ben.li <53158397+liberaterb@users.noreply.github.com>
2021-01-24 09:06:43 +0800

    Update 24.js

--- note/js-note/2021/01/24.js
+++ note/js-note/2021/01/24.js
@@ -1,10 +1,17 @@
-var text20210121_00 = 
+var text20210124_00 = 
 `
 # Vue composition-api
 ## setup option 方法
 ### ref 定义响应式数据 : number string boolean Array
 ### reactive 定义响应式对象（data）
 ### toRefs 对象解构 解构reactive对象
+
+### computed
+* let name = computed(() => prefix + suffix)
+### readonly
+### watchEffect 监听方法体内的响应式数据，初始化也会执行一次
+### watch 与传统的watch一样
+> watch只能监听reactive定义的响应式对象，而watchEffect可以精确到对象里的属性
 
 
 `
